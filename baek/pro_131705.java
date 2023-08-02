4fc80e8a9aa39e841c376d80a094e718994765e6
mal1ing <ghfkd1235@naver.com>
2023-08-02 23:28:51 +0900

    프로그래머스 삼총사 답

---
+++ baek/pro_131705.java
@@ -0,0 +1,20 @@
+package baek;
+
+public class pro_131705 {
+    public static void main(String[] args) {
+        int[] number = {-3, -2, -1, 0, 1, 2 , 3};
+        int cnt = 0;
+        for(int i = 0; i < number.length - 2; i++){
+            for(int j = i + 1; j < number.length - 1; j++){
+                for(int k = j + 1; k < number.length; k++){
+                    int sum = number[i] + number[j] + number[k];
+                    if(sum == 0){
+                        cnt++;
+                    }
+//                    System.out.println("i : " + i + " j : " + j + " k : " + k + " sum : " + sum);
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+}
