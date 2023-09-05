93deacaa64b8791abfae2ca748343555a8ba660e
mal1ing <ghfk1235@naver.com>
2023-09-06 00:13:40 +0900

    0905 큐 처음 사용해봄 Queue FIFO만 기억하자

---
+++ baek/baek_2164.java
@@ -0,0 +1,28 @@
+package baek;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class baek_2164 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Queue<Integer> q = new LinkedList<>();
+
+        int num1 = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i <= num1; i++){
+                q.offer(i);
+        }
+
+        while (q.size() > 1){
+            q.poll();
+            q.offer(q.poll());
+        }
+
+        System.out.println(q.poll());
+    }
+}
